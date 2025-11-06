21015f6f18e0fc337e37afed67025b624f274ce1
chuvaket <thestevenmaps1@gmail.com>
2025-11-06 23:01:39 +0300

    Update pixel_paint_8x8.py
    The "fill" function can now only paint a specific area.
    Fixed indexes error.

--- pixel_paint_8x8.py
+++ pixel_paint_8x8.py
@@ -15,18 +15,43 @@ line = [f'{Back.BLUE}  ',
 
 
 def paint(color):
+    painted_pixel = f'{Back.__getattribute__(color.upper())}  '
+
     if int(pixel_list[0]) == len(line):
-        img[int(pixel_list[1])-1][int(pixel_list[0])-1] = f'{Back.__getattribute__(color.upper())}   ' + f'{Back.RESET}'
+        img[int(pixel_list[1])-1][int(pixel_list[0])-1] = painted_pixel + f' {Back.RESET}'
     else:
-        img[int(pixel_list[1])-1][int(pixel_list[0])-1] = f'{Back.__getattribute__(color.upper())}  '
+        img[int(pixel_list[1])-1][int(pixel_list[0])-1] = painted_pixel
 
 def fill(color, image):
-    for i in range(1, len(line)+1):
-        if i == len(line):
-            line[i-1] = f'{Back.__getattribute__(color.upper())}   ' + f'{Back.RESET}'
-        else:
-            line[i-1] = f'{Back.__getattribute__(color.upper())}  '
-    image = [line[:], line[:], line[:], line[:], line[:], line[:], line[:], line[:]]
+    area = input("Specify the fill borders (x x y y) (press Enter to fill all): ")
+
+    area_pos = list(area) # removes all extra spaces in any line
+    for i in area_pos:
+        if i == " ":
+            area_pos.remove(i)
+
+    area_pos_x = area_pos[:2]
+    area_pos_y = area_pos[2:]
+
+    if area == "":
+        painted_pixel = f'{Back.__getattribute__(color.upper())}  '
+
+        for i in range(1, len(line)+1):
+            if i == len(line):
+                line[i-1] = painted_pixel + f' {Back.RESET}'
+            else:
+                line[i-1] = painted_pixel
+        image = [line[:], line[:], line[:], line[:], line[:], line[:], line[:], line[:]]
+    else:
+        if ((1 <= int(area_pos_x[0]) <= len(line)) & (1 <= int(area_pos_x[1]) <= len(line))) & ((1 <= int(area_pos_y[0]) <= len(line)) & (1 <= int(area_pos_y[0]) <= len(line))):
+            for  i in range(int(area_pos_y[0]), int(area_pos_y[1])+1):
+                if int(area_pos_x[1]) == len(line):
+                    image[i-1][int(area_pos_x[0])-1:int(area_pos_x[1])] = [f'{Back.__getattribute__(color.upper())}  '] * (int(area_pos_x[1]) - int(area_pos_x[0]))
+                    image[i-1][int(area_pos_x[1]):] = [f'{Back.__getattribute__(color.upper())}  ' + f' {Back.RESET}']
+                else:
+                    image[i-1][int(area_pos_x[0])-1:int(area_pos_x[1])] = [f'{Back.__getattribute__(color.upper())}  '] * (int(area_pos_x[1]) + 1 - int(area_pos_x[0]))
+        else: 
+            return image
     return image
 
 
@@ -44,7 +69,7 @@ while True:
     for line in img:
         print(*line, end='\n')
 
-    changed_pixel = input("Enter pixel(x y color): ")
+    changed_pixel = input("Enter the pixel(x y color):")
     if changed_pixel == "exit":
         break
 
@@ -55,10 +80,11 @@ while True:
 
     xy_pixel = changed_pixel[:changed_pixel.rindex(" ")]
 
+
     if xy_pixel == "fill":
         match color:
             case "red":
-                img = fill(color,image=img)
+                img = fill(color,img)
             case "yellow":
                 img = fill(color,img)
             case "black":
@@ -80,32 +106,36 @@ while True:
             if i == " ":
                 pixel_list.remove(i)
 
-        pixel_str = ""
+        if (1 <= int(pixel_list[0]) <= len(line)) & (1 <= int(pixel_list[1]) <= len(line)):
 
-        for i in pixel_list:
-            pixel_str += i
+            pixel_str = ""
 
-        xy_pixel = pixel_str
+            for i in pixel_list:
+                pixel_str += i
 
-        try:
-            int(xy_pixel)
-        except ValueError:
+            xy_pixel = pixel_str
+
+            try:
+                int(xy_pixel)
+            except ValueError:
+                continue
+            else:
+                match color:
+                    case "yellow":
+                        paint(color)
+                    case "white":
+                        paint(color)
+                    case "black":
+                        paint(color)
+                    case "blue":
+                        paint(color)
+                    case "red":
+                        paint(color)
+                    case "green":
+                        paint(color)
+                    case "magenta":
+                        paint(color)
+                    case "cyan":
+                        paint(color)
+        else: 
             continue
-        else:
-            match color:
-                case "yellow":
-                    paint(color)
-                case "white":
-                    paint(color)
-                case "black":
-                    paint(color)
-                case "blue":
-                    paint(color)
-                case "red":
-                    paint(color)
-                case "green":
-                    paint(color)
-                case "magenta":
-                    paint(color)
-                case "cyan":
-                    paint(color)
